76dee3a103540269f51518edf3904956f8bd70e8
elismar <infotec.elismar@gmail.com>
2022-11-17 21:21:27 -0300

    teste

--- src/main/java/senai/org/prjjava/controller/UsuarioController.java
+++ src/main/java/senai/org/prjjava/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,8 @@ import senai.org.prjjava.repository.UsuarioRepository;
 // nesse exemplo são usadas variáveis de parâmetro
 @Controller
 @RequestMapping(path = "/api/usuario")
+@CrossOrigin("*")
+
 public class UsuarioController {
 
         @Autowired
